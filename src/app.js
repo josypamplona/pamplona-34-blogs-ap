fea027781f034a1d31d081adef6ee902734f9fe1
josypamplona <josypamplona@hotmail.com>
2024-01-29 08:43:06 -0300

    requisito 01

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const { rotaDeLogin,
-  rotadeCategoria,
-  rotasPost,
-  rotaDeUsuario,
-} = require('./router/index');
-const { controllers } = require('../src/controller/index');
+// const { userController, 
+//   postController, 
+//   loginController, 
+//   categoriesController } = require('./controller/index');
 
-const erroQuinhentos = require('./middlewares/InternalServerErrorQuinhentos');
+// const erroQuinhentos = require('./middlewares/InternalServerErrorQuinhentos');
 
 const app = express();
 // não remova ou mova esse endpoint
@@ -15,11 +13,11 @@ app.get('/', (_request, response) => {
 });
 app.use(express.json());
 
-app.use('/login', rotaDeLogin, controllers);
-app.use('/user', rotadeCategoria, controllers);
-app.use('/categories', rotaDeUsuario, controllers);
-app.use('/post', rotasPost, controllers);
-app.use(erroQuinhentos);
+// app.use('/login', rotaDeLogin);
+// app.use('/user', rotadeCategoria);
+// app.use('/categories', rotaDeUsuario);
+// app.use('/post', rotasPost);
+// app.use(erroQuinhentos);
 // ...
 
 // É importante exportar a constante `app`,
